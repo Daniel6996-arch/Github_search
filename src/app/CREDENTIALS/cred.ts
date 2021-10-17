ff8b64acb6939899871895945995113a04eebb79
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-10-17 23:53:21 +0300

    add initial files to page

--- src/app/CREDENTIALS/cred.ts
+++
@@ -1,2 +0,0 @@
-export const CLIENT_ID ='Iv1.c319eb1e5e9feb4e'
-export const CLIENT_SECRET ='0ff3fcbe579910fcc43fdd600a4fa429f794d3f2'
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,44 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { CLIENT_ID, CLIENT_SECRET } from '../CREDENTIALS/cred';
-import { Observable, throwError } from 'rxjs';
-import { HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor(private httpClient:HttpClient) { }
-   
-   //for github profile
-   public getProfile(searchQuery:any):Observable<any>{
-     let dataURL = 'https://api.github.com/users/${searchQuery}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}';
-     return this.httpClient.get<any>(dataURL).pipe(
-       retry(count 1),
-       catchError(this.handleErrors)
-     )
-   }
-   public handleErrors(error:HttpErrorResponse){
-     let errorMessage:string;
-     if(error.error instanceof ErrorEvent){
-       errorMessage = 'MESSAGE: ${error.error.message}';
-     }
-     else{
-       errorMessage = 'STATUS: ${error.status} MESSAGE : ${error.message}';
-     }
-     return throwError(errorMessage)
-   }
-
- //get github repos
-   public getRepos(searchQuery:any):Observable<any[]>{
-    let dataURL = 'https://api.github.com/users/${searchQuery}/repos?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}';
-    return this.httpClient.get<any>(dataURL).pipe(
-      //retry(count 1),
-      catchError(this.handleErrors)
-    )
-  }
-
+  constructor(private) { }
 }
- 
