31cf65bc0144c2a6dfa21836e5f44db83cae48a9
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-10-17 22:48:31 +0300

    add initial files to page

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { CLIENT_ID, CLIENT_SECRET } from '../CREDENTIALS/cred';
+import { throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +15,20 @@ export class GithubService {
    //for github profile
    public getProfile(searchQuery){
      let dataURL = 'https://api.github.com/users/${searchQuery}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}';
+     return this.httpClient.get(dataURL).pipe(
+       retry(count 1),
+       catchError(this.handleErrors)
+     )
+   }
+   public handleErrors(error:HttpErrorResponse){
+     let errorMessage:string;
+     if(error.error instanceof ErrorEvent){
+       errorMessage = 'MESSAGE: ${error.error.message}';
+     }
+     else{
+       errorMessage = 'STATUS: ${error.status} MESSAGE : ${error.message}';
+     }
+     return throwError(errorMessage)
    }
 
 }
