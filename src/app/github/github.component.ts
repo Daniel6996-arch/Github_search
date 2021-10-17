220f15f50d2d19c771c4b0ff6b75b89c1c7dd2ac
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-10-18 00:16:57 +0300

    add initial files to page

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubComponent implements OnInit {
 
-users?:String[]
+users?:any[]
 
 
   constructor(private githubService:GithubService) { }
