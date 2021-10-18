c2f4ef860d2679cbc6fe23633e6d5d26908ca6aa
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-10-18 03:40:40 +0300

    add initial search forms to page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
 import { MyUserComponent } from './my-user/my-user.component';
 import { ReposComponent } from './repos/repos.component';
 
 const routes: Routes = [
-  {path: 'home', component:AppComponent},
-  {path: 'mygit', component:MyUserComponent},
+  {path: 'home', component:MyUserComponent},
   {path: 'users', component:GithubComponent},
   {path: 'repos', component:ReposComponent}
 ];
